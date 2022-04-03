ac3f14755980b361d3b1acd0861d5280591e10f3
Jacob Valenzuela <valen306@cougars.csusm.edu>
2022-04-03 09:40:03 -0700

    added TODOs in CMM_Util.java

--- src/main/java/model/CMM_Util.java
+++ src/main/java/model/CMM_Util.java
@@ -4,13 +4,11 @@ import java.math.BigDecimal;
 
 public class CMM_Util {
     public static final BigDecimal REGULAR = new BigDecimal(1.0);
-//    public final BigDecimal MOCHA = new BigDecimal();
-//    public final BigDecimal LATTE = new BigDecimal();
-//    public final BigDecimal ESPRESSO = new BigDecimal();
-//    public final BigDecimal CAPPUCCINO = new BigDecimal();
+    // TODO: ADD MORE COFFEE TYPES
     public static final BigDecimal CREAM = new BigDecimal(.25);
-
+    // TODO: ADD MORE ADD-ONS
     public static final String REGULAR_ACTION_COMMAND = "REGULAR";
     public static final String MOCHA_ACTION_COMMAND = "MOCHA";
+    // TODO: ADD MORE ACTION COMMANDS
 
 }
